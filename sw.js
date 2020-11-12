fc1403af199921cd783e366098d539c2c2a48b2b
Margaret <mkiuur@gmail.com>
2020-11-12 13:54:18 -0500

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6c904b7826d3464cc737.js"
+    "url": "webpack-runtime-ceae5432c8eee8131211.js"
   },
   {
     "url": "framework-81279cacaab4bdba7685.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "cd07367bc4bf0abe2a9207384bbe77c7"
+    "revision": "ec41153a4cee98fefd27979b896a4a01"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-ad6431e4664bcf916d19.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "bd4543e707a32a5b1bfea0ac76782b07"
+    "revision": "758f91bb0928c2d6aa2c218e5da28c56"
   },
   {
     "url": "polyfill-1d6410031586e9804baf.js"
