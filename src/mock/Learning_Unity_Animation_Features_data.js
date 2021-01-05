a2da657bbbe6baf0430aaa85ad1eedaeb8086fd1
Margaret <mkiuur@gmail.com>
2021-01-05 16:24:36 -0500

    reversed progress lists for witch, animation pages

--- src/mock/Learning_Unity_Animation_Features_data.js
+++ src/mock/Learning_Unity_Animation_Features_data.js
@@ -18,10 +18,9 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'Work_Photo.jpg',
-  paragraphOne:
-    'Learning Unity Animation Features',
+  paragraphOne: 'Learning Unity Animation Features',
   paragraphTwo:
-    'I have recently been learning how to use Unity\'s animation features. Check out my progress!',
+    "I have recently been learning how to use Unity's animation features. Check out my progress!",
   paragraphThree: '',
   resume: 'https://drive.google.com/file/d/13_lCtgol_VL0ybbYB13QqvnAUJ7H592x/view?usp=sharing', // if no resume, the button will not show up
 };
@@ -30,40 +29,21 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'Boolean Animation Controller.gif',
-    title: 'Boolean Animation Controller',
+    img: 'Animation Layers.gif',
+    title: 'Animation Layers',
     info:
-      'Animations are changed based on boolean values. e.g. if \'w\' is pressed, she will walk, if \'shift\' is pressed, she will run.',
+      'By creating an mask of the humanoid rig, a separate animation can be used for different parts of the body. ',
     info2:
-      'Switching between walking and running animations is immediate and choppy looking.',
+      'An example of this would be using an upper body mask for an idle pistol holding animation while the lower body continues to play a walking or running animation.',
     url: '',
-    repo: 'https://github.com/mkiuur/Learning-Unity-Animation-Features/blob/main/animationStateController.cs', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: '1D Blend Trees.gif',
-    title: '1D Blend Trees',
-    info:
-      'The animations for walking and running are blended together based on a float value for velocity in the X direction. The longer \'w\' is pressed, the greater the velocity.',
-    info2:
-      'As the velocity increases, the running animation will be further blended into the walking animation.',
-    url: '',
-    repo: 'https://github.com/mkiuur/Learning-Unity-Animation-Features/blob/main/animationStateControllerBlend.cs', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: '2D Blend Trees.gif',
-    title: '2D Blend Trees',
-    info: 'The walking and running animations are now blended with a right strafe and left strafe animation depending on the velocity float in the Z direction.',
-    info2: 'The X and Z velocities are locked and reset at 0.5f for walking and 2.0f for running based on whether the \'shift\' key is being pressed.',
-    url: '',
-    repo: 'https://github.com/mkiuur/Learning-Unity-Animation-Features/blob/main/twoDimensionalAnimationStateController.cs', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
     img: 'Animation Retargetting Before.gif',
     title: 'Animation Retargetting Before',
-    info: 'When starting to retarget animations to other characters, because the animations were not mapped to humanoid characters, there were some unexpected results...',
+    info:
+      'When starting to retarget animations to other characters, because the animations were not mapped to humanoid characters, there were some unexpected results...',
     info2: '',
     url: '',
     repo: '', // if no repo, the button will not show up
@@ -72,19 +52,46 @@ export const projectsData = [
     id: nanoid(),
     img: 'Animation Retargetting After.gif',
     title: 'Animation Retargetting After',
-    info: 'After mapping all the animations to be humanoid using Unity\'s humanoid animation retargetting system, the animations work as expected for all humanoid rigs.',
+    info:
+      "After mapping all the animations to be humanoid using Unity's humanoid animation retargetting system, the animations work as expected for all humanoid rigs.",
     info2: '',
     url: '',
     repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'Animation Layers.gif',
-    title: 'Animation Layers',
-    info: 'By creating an mask of the humanoid rig, a separate animation can be used for different parts of the body. ',
-    info2: 'An example of this would be using an upper body mask for an idle pistol holding animation while the lower body continues to play a walking or running animation.',
+    img: '2D Blend Trees.gif',
+    title: '2D Blend Trees',
+    info:
+      'The walking and running animations are now blended with a right strafe and left strafe animation depending on the velocity float in the Z direction.',
+    info2:
+      "The X and Z velocities are locked and reset at 0.5f for walking and 2.0f for running based on whether the 'shift' key is being pressed.",
     url: '',
-    repo: '', // if no repo, the button will not show up
+    repo:
+      'https://github.com/mkiuur/Learning-Unity-Animation-Features/blob/main/twoDimensionalAnimationStateController.cs', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: '1D Blend Trees.gif',
+    title: '1D Blend Trees',
+    info:
+      "The animations for walking and running are blended together based on a float value for velocity in the X direction. The longer 'w' is pressed, the greater the velocity.",
+    info2:
+      'As the velocity increases, the running animation will be further blended into the walking animation.',
+    url: '',
+    repo:
+      'https://github.com/mkiuur/Learning-Unity-Animation-Features/blob/main/animationStateControllerBlend.cs', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'Boolean Animation Controller.gif',
+    title: 'Boolean Animation Controller',
+    info:
+      "Animations are changed based on boolean values. e.g. if 'w' is pressed, she will walk, if 'shift' is pressed, she will run.",
+    info2: 'Switching between walking and running animations is immediate and choppy looking.',
+    url: '',
+    repo:
+      'https://github.com/mkiuur/Learning-Unity-Animation-Features/blob/main/animationStateController.cs', // if no repo, the button will not show up
   },
 ];
 
--- src/mock/witch_rhythm_game_data.js
+++ src/mock/witch_rhythm_game_data.js
@@ -18,10 +18,9 @@ export const heroData = {
 // ABOUT DATA
 export const aboutData = {
   img: 'Work_Photo.jpg',
-  paragraphOne:
-  'Witch Rhythm Game',
+  paragraphOne: 'Witch Rhythm Game',
   paragraphTwo:
-  'Create helpful potions by casting spells along to music! The purpose of this project is further my knowledge about game development and Unity\'s animation features.',
+    "Create helpful potions by casting spells along to music! The purpose of this project is further my knowledge about game development and Unity's animation features.",
   paragraphThree: '',
   resume: 'https://drive.google.com/file/d/13_lCtgol_VL0ybbYB13QqvnAUJ7H592x/view?usp=sharing', // if no resume, the button will not show up
 };
@@ -30,32 +29,22 @@ export const aboutData = {
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'witch mini mood board.jpg',
-    title: 'Concept',
+    img: 'particle effects, spin animations.gif',
+    title: 'Particle effects and spinning card animations',
     info:
-    'The basic idea for the game is that a little witch creates potions by casting spells along to music. ',
+      'I added some particle effects and trails to the projectiles to make them look like they were flying as apposed to transforming. I also made the box colliders more card shaped and gave them a spin animation for when they are hit.',
     info2:
-    'Projectiles (spells) would fly towards hit boxes (magic cards) for the rhythm game. The witch asset would perform different animations based on the key sequence behind the main gameplay.',
+      'More cards were added so that a player has to incorporate multiple key presses to hit diagonal cards.',
     url: '',
-    repo: '', // if no repo, the button will not show up
+    repo: 'https://github.com/mkiuur/Witch-Rhythm-Game', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: '2D rhythm game tutorial trimmed.gif',
-    title: 'Rhythm Game Tutorial',
+    img: '3d rhythm game circle config trimmed.gif',
+    title: 'Circle Configuration',
     info:
-    'I followed a tutorial on how to make a basic rhythm game in Unity and reproduced the example.',
-    info2:
-    'Through this I learned most of the basic skills I would need moving forward.',
-    url: '',
-    repo: '', // if no repo, the button will not show up
-  },
-  {
-    id: nanoid(),
-    img: '2D rhythm game with pooling trimmed.gif',
-    title: 'Object Pooling',
-    info: 'Anticipating a large amount of potential instantiations and destructions, I implemented an object pooling system for the projectiles to reuse objects.',
-    info2: 'I also started looking into user interface elements.',
+      'In my original concept I wanted the rhythm game to have a circular configuration. This makes it more difficult becuase it is harder to judge the consecutive order of the required key presses.',
+    info2: 'Down the line I also want to add circular witch themed art.',
     url: '',
     repo: '', // if no repo, the button will not show up
   },
@@ -70,21 +59,34 @@ export const projectsData = [
   },
   {
     id: nanoid(),
-    img: '3d rhythm game circle config trimmed.gif',
-    title: 'Circle Configuration',
-    info: 'In my original concept I wanted the rhythm game to have a circular configuration. This makes it more difficult becuase it is harder to judge the consecutive order of the required key presses.',
-    info2: 'Down the line I also want to add circular witch themed art.',
+    img: '2D rhythm game with pooling trimmed.gif',
+    title: 'Object Pooling',
+    info:
+      'Anticipating a large amount of potential instantiations and destructions, I implemented an object pooling system for the projectiles to reuse objects.',
+    info2: 'I also started looking into user interface elements.',
     url: '',
     repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'particle effects, spin animations.gif',
-    title: 'Particle effects and spinning card animations',
-    info: 'I added some particle effects and trails to the projectiles to make them look like they were flying as apposed to transforming. I also made the box colliders more card shaped and gave them a spin animation for when they are hit.',
-    info2: 'More cards were added so that a player has to incorporate multiple key presses to hit diagonal cards.',
+    img: '2D rhythm game tutorial trimmed.gif',
+    title: 'Rhythm Game Tutorial',
+    info:
+      'I followed a tutorial on how to make a basic rhythm game in Unity and reproduced the example.',
+    info2: 'Through this I learned most of the basic skills I would need moving forward.',
     url: '',
-    repo: 'https://github.com/mkiuur/Witch-Rhythm-Game', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
+  },
+  {
+    id: nanoid(),
+    img: 'witch mini mood board.jpg',
+    title: 'Concept',
+    info:
+      'The basic idea for the game is that a little witch creates potions by casting spells along to music. ',
+    info2:
+      'Projectiles (spells) would fly towards hit boxes (magic cards) for the rhythm game. The witch asset would perform different animations based on the key sequence behind the main gameplay.',
+    url: '',
+    repo: '', // if no repo, the button will not show up
   },
 ];
 
